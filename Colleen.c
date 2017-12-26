aeea76da7ba398deba733e1992ba90e45c61ef53
Chayaline <cha.moncoucut@gmail.com>
2017-12-26 17:03:16 +0100

    cleanup and opti of colleen

--- Colleen.c
+++ Colleen.c
@@ -10,11 +10,9 @@ int main (void) {
 /*
     ceci est un autre commentaire
 */
-    char q = '"';
-    char *s1="#include <stdio.h>%c/*%c%sceci est un commentaire%c*/%cchar *spaces(){%c%sreturn %c    %c;%c}%c%c";
-    char *print="printf(";
-    char *s2="int main (void) {%c/*%c%sceci est un autre commentaire%c*/%c%schar q = '%c';%c%schar *s1=%c%s%c;%c%schar *print=%c%s%c;%c%schar *s2=%c%s%c;%c%s%ss1, 10, 10, spaces(), 10, 10,  10, spaces(), q, q, 10, 10, 10);%c%s%ss2, 10, 10, spaces(), 10, 10, spaces(), q, 10, spaces(), q, s1, q, 10, spaces(), q, print, q, 10, spaces(), q, s2, q, 10, spaces(), print, 10, spaces(), print, 10, spaces(), 10, 10, spaces(), 10);%c%sreturn (0);%c}";
-    printf(s1, 10, 10, spaces(), 10, 10,  10, spaces(), q, q, 10, 10, 10);
-    printf(s2, 10, 10, spaces(), 10, 10, spaces(), q, 10, spaces(), q, s1, q, 10, spaces(), q, print, q, 10, spaces(), q, s2, q, 10, spaces(), print, 10, spaces(), print, 10, spaces(), 10, 10, spaces(), 10);
+    char *s1="#include <stdio.h>%1$c/*%1$c%2$sceci est un commentaire%1$c*/%1$cchar *spaces(){%1$c%2$sreturn %3$c    %3$c;%1$c}%1$c%1$c";
+    char *s2="int main (void) {%1$c/*%1$c%2$sceci est un autre commentaire%1$c*/%1$c%2$schar *s1=%3$c%4$s%3$c;%1$c%2$schar *s2=%3$c%5$s%3$c;%1$c%2$sprintf(s1, 10, spaces(), 34);%1$c%2$sprintf(s2, 10, spaces(), 34, s1, s2);%1$c%2$sreturn (0);%1$c}";
+    printf(s1, 10, spaces(), 34);
+    printf(s2, 10, spaces(), 34, s1, s2);
     return (0);
 }
